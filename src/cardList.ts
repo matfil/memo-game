334114e7f24de7fbd3316fea721b59c9c510ad99
Mateusz Filipiuk <mateusz.filipiuk@gmail.com>
2025-03-21 18:08:31 +0100

    deal cards

--- src/cardList.ts
+++ src/cardList.ts
@@ -1,17 +1,18 @@
 export interface Card {
-    id: number;
+    index: number;
     src: string;
     isFlipped: boolean;
     isMatched: boolean;
+    id?: number;
 }
 
 const cardList : Card[] = [
-    {id: 1, src: "images/1.png", isFlipped: false, isMatched: false},
-    {id: 2, src: "images/2.png", isFlipped: false, isMatched: false},
-    {id: 3, src: "images/3.png", isFlipped: false, isMatched: false},
-    {id: 4, src: "images/4.png", isFlipped: false, isMatched: false},
-    {id: 5, src: "images/5.png", isFlipped: false, isMatched: false},
-    {id: 6, src: "images/6.png", isFlipped: false, isMatched: false},
+    {index: 1, src: "images/1.png", isFlipped: false, isMatched: false},
+    {index: 2, src: "images/2.png", isFlipped: false, isMatched: false},
+    {index: 3, src: "images/3.png", isFlipped: false, isMatched: false},
+    {index: 4, src: "images/4.png", isFlipped: false, isMatched: false},
+    {index: 5, src: "images/5.png", isFlipped: false, isMatched: false},
+    {index: 6, src: "images/6.png", isFlipped: false, isMatched: false},
 ];
 
 export default cardList;
--- src/hooks/gameState.ts
+++ src/hooks/gameState.ts
@@ -1,59 +1,92 @@
 import { create } from "zustand";
-import { Card } from "../cardList";
+import cardList, { Card } from "../cardList";
 
-export interface Coords {
-    x: number;
-    y: number;
-}
 export interface move {
-    coords1: Coords;
-    coords2: Coords;
+  card1: Card;
+  card2: Card;
 }
 
 export interface GameState {
-    moves: move[];
-    setMoves: (moves: move[]) => void;
-    addMove: (move: move) => void;
-    cards: Card[];
-    setCards: (cards: Card[]) => void;
-    gameActive: boolean;
-    setGameActive: (gameActive: boolean) => void;
-    attempts: number;
-    incrementAttempts: () => void;
-    difficulty: number;
-    setDifficulty: (difficulty: number) => void;
-    time: number;
-    setTime: (time: number) => void;
-    incrementTime: () => void;
-    timeInterval: number;
-    startTimer: () => void;
-    stopTimer: () => void;
-    startGame: () => void;
-    stopGame: () => void;
+  moves: move[];
+  setMoves: (moves: move[]) => void;
+  addMove: (move: move) => void;
+  cards: Card[];
+  setCards: (cards: Card[]) => void;
+  gameActive: boolean;
+  setGameActive: (gameActive: boolean) => void;
+  attempts: number;
+  incrementAttempts: () => void;
+  difficulty: number;
+  setDifficulty: (difficulty: number) => void;
+  time: number;
+  setTime: (time: number) => void;
+  incrementTime: () => void;
+  timeInterval: number;
+  startTimer: () => void;
+  stopTimer: () => void;
+  startGame: () => void;
+  stopGame: () => void;
+  newCardSet: () => void;
+}
 
+function shuffle(array: Card[]): Card[] {
+  return array.sort(() => Math.random() - 0.5);
 }
 
 const useGameState = create<GameState>((set, get) => ({
-    moves: [],
-    setMoves: (moves:move[]) => set({ moves }),
-    addMove: (move:move) => set((state:GameState) => ({ moves: [...state.moves, move] })),
-    cards: [],
-    setCards: (cards:Card[]) => set({ cards }),
-    gameActive: false,
-    setGameActive: (gameActive:boolean) => set({ gameActive }),
-    attempts: 0,
-    incrementAttempts: () => set((state:GameState) => ({ attempts: state.attempts + 1 })),
-    difficulty: 12,
-    setDifficulty: (difficulty:number) => {console.log(difficulty); set({ difficulty })},
-    time: 0,
-    incrementTime: () => set((state:GameState) => ({ time: state.time + 1 })),
-    setTime: (time:number) => set({ time }),
-    timeInterval: 0,
-    startTimer: () => set((state:GameState) => ({ timeInterval: window.setInterval(() => state.incrementTime(), 1000) })),
-    stopTimer: () => set((state:GameState) => { clearInterval(state.timeInterval); return { time: 0, timeInterval: 0 } }),
-    startGame: () => { if(get().gameActive) {{get().setGameActive(true); get().startTimer();}}},
-    stopGame: () => { get().setGameActive(false); get().stopTimer(); },
+  moves: [],
+  setMoves: (moves: move[]) => set({ moves }),
+  addMove: (move: move) =>
+    set((state: GameState) => ({ moves: [...state.moves, move] })),
+  cards: [],
+  setCards: (cards: Card[]) => set({ cards }),
+  gameActive: false,
+  setGameActive: (gameActive: boolean) => set({ gameActive }),
+  attempts: 0,
+  incrementAttempts: () =>
+    set((state: GameState) => ({ attempts: state.attempts + 1 })),
+  difficulty: 12,
+  setDifficulty: (difficulty: number) => {
+    console.log(difficulty);
+    set({ difficulty });
+  },
+  time: 0,
+  incrementTime: () => set((state: GameState) => ({ time: state.time + 1 })),
+  setTime: (time: number) => set({ time }),
+  timeInterval: 0,
+  startTimer: () =>
+    set((state: GameState) => ({
+      timeInterval: window.setInterval(() => state.incrementTime(), 1000),
+    })),
+  stopTimer: () =>
+    set((state: GameState) => {
+      clearInterval(state.timeInterval);
+      return { time: 0, timeInterval: 0 };
+    }),
+  startGame: () => {
+    if (get().gameActive) {
+      {
+        get().newCardSet();
+        get().setMoves([]);
+        get().setGameActive(true);
+        get().startTimer();
+      }
+    }
+  },
+  stopGame: () => {
+    get().setGameActive(false);
+    get().stopTimer();
+  },
+  newCardSet: () => {
+    const randomCards = shuffle(cardList).slice(0, get().difficulty / 2);
 
+    set({
+      cards: [...randomCards, ...randomCards].map((card, index) => ({
+        ...card,
+        id: index,
+      }))
+    });
+  },
 }));
 
 export default useGameState;
